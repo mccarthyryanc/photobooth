0d876f8f31ff9e0082c174585e4102ab24609061
Ryan PI <ryan.schilt@gmail.com>
2014-01-19 21:41:36 +0000

    Adding test script for camera

---
+++ api_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# Testing script for the raspicam API
+#
+
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    camera.start_recording('foo.h264')
+    camera.wait_recording(20)
+    camera.stop_recording()
+    camera.stop_preview()
