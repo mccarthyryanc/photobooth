d0270d63f9feb724921a11576f9c9a81dcd0ed5b
Ryan Desktop <ryan.schilt@gmail.com>
2014-01-19 13:44:01 -0800

    General testing

--- api_test.py
+++ api_test.py
@@ -6,10 +6,18 @@
 import time
 import picamera
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
+# with picamera.PiCamera() as camera:
+#     camera.resolution = (640, 480)
+#     camera.start_preview()
+#     camera.start_recording('foo.h264')
+#     camera.wait_recording(20)
+#     camera.stop_recording()
+#     camera.stop_preview()
+
+camera = picamera.PiCamera()
+try:
     camera.start_preview()
-    camera.start_recording('foo.h264')
-    camera.wait_recording(20)
-    camera.stop_recording()
+    time.sleep(10)
     camera.stop_preview()
+finally:
+    camera.close()
