3561ee010af0c6e822d6fde3c5d169697f6bad42
Ryan Desktop <ryan.schilt@gmail.com>
2014-01-19 14:16:16 -0800

    General testing

--- api_test.py
+++ api_test.py
@@ -3,8 +3,25 @@
 # Testing script for the raspicam API
 #
 
+import io
 import time
 import picamera
+import cv2
+import numpy as np
+
+# Create the in-memory stream
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture(stream, format='jpeg')
+# Construct a numpy array from the stream
+data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+# "Decode" the image from the array, preserving colour
+image = cv2.imdecode(data, 1)
+# OpenCV returns an array with data in BGR order. If you want RGB instead
+# use the following...
+image = image[:, :, ::-1]
 
 # camera = picamera.PiCamera()
 # try:
@@ -14,10 +31,10 @@ import picamera
 # finally:
 #     camera.close()
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    camera.start_recording('foo.h264', inline_headers=False)
-    camera.wait_recording(20)
-    camera.stop_recording()
-    camera.stop_preview()
+# with picamera.PiCamera() as camera:
+#     camera.resolution = (640, 480)
+#     camera.start_preview()
+#     camera.start_recording('foo.h264', inline_headers=False)
+#     camera.wait_recording(20)
+#     camera.stop_recording()
+#     camera.stop_preview()
