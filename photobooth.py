51b7fe6e176172b97bc3135c3dca588e4f0f4f09
Ryan PI <ryan.schilt@gmail.com>
2014-07-21 07:00:41 +0000

    Now have a functional photobooth. Need to work a bit on performance now...

--- photobooth.py
+++ photobooth.py
@@ -7,21 +7,24 @@ import time
 import picamera
 import RPi.GPIO as GPIO
 from datetime import datetime
+from subprocess import Popen, PIPE
+
 
 # GPIO pin constants
-led_pin = 11
-green_button_pin = 16
-red_button_pin = 18
+led_pin = 17
+green_button_pin = 24
+red_button_pin = 23
 
 # === PIN Setups ===
+GPIO.setwarnings(False)
 # GPIO.setmode(GPIO.BOARD)
 GPIO.setmode(GPIO.BCM)
 # Button setup for "falling edge" detection on press
 GPIO.setup(green_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(red_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(red_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Led steup
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.output(led_pin, GPIO.HIGH)
+# GPIO.setup(led_pin, GPIO.OUT)
+# GPIO.output(led_pin, GPIO.HIGH)
 
 # === General LED Constants ===
 # state - decides what LED should be on and off
@@ -33,9 +36,13 @@ gpio_refresh_time = 0.1
 
 # === Picture constants ===
 max_pic = 4     # Max number of pics
-cam_rest_time = 3   # seconds
+rest_time = 2   # seconds
 pic_extn = ".jpg"
-filelist = ["image0%d"%num for num in range(max_pic)]
+filelist = ["image0%d%s"%(num,pic_extn) for num in range(max_pic)]
+
+global print_waiting, external_drive
+
+external_drive = "/media/B64D-7D76"
 print_waiting = True
 
 
@@ -51,8 +58,8 @@ def blink(pin_number, length, rate):
     # for amount of length
     if rate < 0:
         GPIO.output(pin_number, GPIO.LOW)
-        sleep(length)
-        GPIO.output(pin_number, GPIO.HIGH)
+        time.sleep(length)
+        #GPIO.output(pin_number, GPIO.HIGH)
         return 0
 
     # rate is positive, start blinking
@@ -64,11 +71,11 @@ def blink(pin_number, length, rate):
 
         if (state > 0):
             GPIO.output(pin_number, GPIO.LOW)
-            print "positive state, GPIO.LOW"
+            #print "positive state, GPIO.LOW"
         else:
             GPIO.output(pin_number, GPIO.HIGH)
-            print "positive negative, GPIO.HIGH"
-        sleep(rate)
+            #print "positive negative, GPIO.HIGH"
+        time.sleep(rate)
 
     return 0
 
@@ -88,17 +95,39 @@ def start_camera():
     Function to start the camera preview and take
     the pictures
     """
-    print datetime.now() , " Taking picture!"
+    print datetime.now() , " | Taking picture!"
     with picamera.PiCamera() as camera:
-        camera.resolution = (1800, 1200)
+        # camera.resolution = (1440, 900)
+        camera.preview_fullscreen = False
+        camera.preview_window = (0,0,1440,900)
         camera.start_preview()
+        camera.resolution = (450,300)
         for img_file in filelist:
             led_countdown_blink()
+            # camera.resolution = (450,300)
             camera.capture(img_file)
+            GPIO.output(led_pin, GPIO.HIGH)
             time.sleep(rest_time)
         camera.stop_preview()
 
-def save_current_image():
+def make_photo():
+    """
+    Function to make the final photo that is saved or printed
+    """
+    sys_cmd = "montage image00.jpg image01.jpg image02.jpg image03.jpg -tile 2x2 -border 5 -geometry 450x300+0+0 combined_image.png"
+    # sys_cmd = "montage image00.jpg image01.jpg -tile 1x2 -border 5 -geometry +0+0 top.jpg && "
+    # sys_cmd += "montage image02.jpg image03.jpg -tile 1x2 -border 5 -geometry +0+0 bottom.jpg && "
+    # sys_cmd += "montage top.jpg bottom.jpg -tile 2x1 -border 5 -geometry +0+0 combined_image.png"
+    # sys_cmd += "convert top_and_bottom.jpg -resize 900x600 combined_image.png"
+    
+    process = Popen(sys_cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = process.communicate()
+    print datetime.now(), " | make_photo out:"
+    print out
+    print datetime.now(), " | make_photo err:"
+    print err
+
+def save_current_image(channel):
     """
     Function callback for the red button. Saves the image
     most recently taken and changes the value of print_waiting
@@ -106,19 +135,41 @@ def save_current_image():
     an image.
     """
     global print_waiting
+    save_cmd = "cp combined_image.png %s/image_%d.png"%(external_drive,int(time.time()))
+    print datetime.now(), " | Saving the image: %s"%save_cmd
+    
+    process = Popen(save_cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = process.communicate()
 
-
+    print datetime.now(), " | save_image out:"
+    print out
+    print datetime.now(), " | save_image err:"
+    print err
+   
     # now finished, stop waiting
     print_waiting = False
 
-def print_current_image():
+def print_current_image(channel):
     """
     Function callback for the green button. Launches
     Subprocess to call print functions and save image
     function.
     """
-    print_cmd = "lp -d Canon_CP900 current_image.png"
-    
+    global print_waiting
+
+    print_cmd = "lp -d Canon_CP900 combined_image.png"
+    print datetime.now(), " | Printing image: %s"%print_cmd
+
+    process = Popen(print_cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = process.communicate()
+
+    print datetime.now(), " | print_photo out:"
+    print out
+    print datetime.now(), " | print_photo err:"
+    print err
+
+    # now finished, stop waiting
+    print_waiting = False
 
 
 if __name__ == '__main__':
@@ -127,24 +178,43 @@ if __name__ == '__main__':
     running = True
     while running:
         try:
-            print datetime.now() , " | Waiting for green putton press..."  
-            GPIO.wait_for_edge(green_button_pin, GPIO.BOTH)
-            # === Start the button listeners for printing/no-printing ===
-            GPIO.add_event_detect(green_button_pin, GPIO.BOTH, callback=print_current_image, bouncetime=300)
-            GPIO.add_event_detect(red_button_pin, GPIO.BOTH, callback=save_current_image, bouncetime=300)
+            # Led steup
+            GPIO.setup(led_pin, GPIO.OUT)
+            GPIO.output(led_pin, GPIO.HIGH)
 
-            # Start the camera and take pictures
+            print datetime.now() , " | Waiting for green putton press..."  
+            GPIO.wait_for_edge(green_button_pin, GPIO.RISING)
+            # Start the camera and take the pictures
             start_camera()
-            # Empty loop while waiting for 
+            make_photo()
+            
+            # === Start the button listeners for printing/no-printing ===
+            GPIO.remove_event_detect(green_button_pin)
+            GPIO.setup(green_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(red_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            
+            GPIO.add_event_detect(green_button_pin, GPIO.RISING, callback=print_current_image, bouncetime=200)
+            GPIO.add_event_detect(red_button_pin, GPIO.FALLING, callback=save_current_image, bouncetime=200)
+            timer_start = time.time()
+            timer_end = timer_start + 60.0
+            
+            # Empty loop while waiting for button press
             print_waiting = True
             print datetime.now() , " | Waiting for Green or Red button press..."
             while print_waiting:
-                continue
+                if timer_end < time.time():
+                    print datetime.now(), " | Hit timer expiration."
+                    save_current_image(-1)
+                    print_waiting = False
 
-
+            # Remove the event listeners so we can start fresh
+            GPIO.remove_event_detect(green_button_pin)
+            GPIO.remove_event_detect(red_button_pin)
+            GPIO.setup(green_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(red_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         except KeyboardInterrupt:
-            print datetime.now() , "Hit ctrl+C, exiting. Good Bye!"
+            print datetime.now() , " | Hit ctrl+C, exiting. Good Bye!"
             running = False
 
     GPIO.cleanup()
